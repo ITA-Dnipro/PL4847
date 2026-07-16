d3fbff417e647733b421b6736506fda37f79ceab
Iryna Sapeliuk <irinasapeluk3676788@gmail.com>
2026-07-16 12:44:51 +0300

    scaffold backend structure

--- backend/project/settings/dev.py
+++ backend/project/settings/dev.py
@@ -2,10 +2,6 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
 
