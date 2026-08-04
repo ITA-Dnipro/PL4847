36383c98799a274f8c9860efb94339f4341bcb77
Dmytro Novykov <dimacpoint@gmail.com>
2026-08-04 09:00:00 +0300

    fix backend formatting for CI

--- backend/project/settings/dev.py
+++ backend/project/settings/dev.py
@@ -6,5 +6,5 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 for docker_host in ("backend", "forum_backend"):
     if docker_host not in ALLOWED_HOSTS:
         ALLOWED_HOSTS.append(docker_host)
-        
+    
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
