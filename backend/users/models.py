c4aa19a46354e1c8ba8988f5b2aa37973f6e4ee7
Iryna Sapeliuk <irinasapeluk3676788@gmail.com>
2026-08-06 13:10:59 +0300

    set default is_active to False for new registered users

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,6 +2,20 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        
+        extra_fields.setdefault("is_active", False)
+        
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
@@ -29,6 +43,9 @@ class User(AbstractUser):
     )
     updated_at = models.DateTimeField(
         auto_now=True,
+    )
+    is_active = models.BooleanField(
+        default=False,
     )
 
     def can_have_startup_profile(self) -> bool:
