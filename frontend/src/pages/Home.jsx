9bd13b41e97a76eda53d817bb4b8e4b0ebab69bd
VolodymyrOleshchuk <volodimirolesuk987@gmail.com>
2026-08-07 14:57:09 +0100

    Add hero section with API fallback and CTA task 14

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import CTASection from "../components/CTASection"
 import ForWhomGrid from "../components/ForWhomGrid"
-
+import Hero from "../components/Hero/Hero"
 
 function Home() {
-  return ( <
-    div>
+  return (
+    <div>
+      <Hero />
       <h1>Home page</h1>
       <CTASection />
       <ForWhomGrid />
