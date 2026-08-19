5c75187b4c1090ae11075b4da407e7a8bb5d0e91
Dmytro Novykov <dimacpoint@gmail.com>
2026-08-20 01:36:28 +0300

    fixed Register page to set footer at the bottom

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
 import Footer from "../components/Footer";
+import './Register.css'
 
 function Register() {
   return (
     <>
+    <div className="register-picker">
       <h1>Register Page</h1>
       <Link to="/register/startup">As startup</Link>  
       <Link to="/register/investor">As investor</Link> 
+    </div>
       <Footer /> 
     </>
   )
