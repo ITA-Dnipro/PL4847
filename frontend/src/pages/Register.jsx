a795cd5be51ec70c24b0b85b65a14f951ccfd3ae
Dmytro Novykov <dimacpoint@gmail.com>
2026-08-20 01:39:16 +0300

    updated register page with options

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,16 +1,24 @@
 import { Link } from "react-router-dom"
-import Footer from "../components/Footer";
-import './Register.css'
+import Footer from "../components/Footer"
+import "./Register.css"
 
 function Register() {
   return (
     <>
-    <div className="register-picker">
-      <h1>Register Page</h1>
-      <Link to="/register/startup">As startup</Link>  
-      <Link to="/register/investor">As investor</Link> 
-    </div>
-      <Footer /> 
+      <div className="register-picker">
+        <h1 className="register-picker__heading">Оберіть тип реєстрації</h1>
+
+        <div className="register-picker__cards">
+          <Link className="register-picker__card" to="/register/startup">
+            <span className="register-picker__card-title">Зареєструватися як стартап</span>
+          </Link>
+
+          <Link className="register-picker__card" to="/register/investor">
+            <span className="register-picker__card-title">Зареєструватися як інвестор</span>
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </>
   )
 }
